be6c8bbb51b9293b2eec59e5ef6aec729e66d7f0
Robert Warren <warrenrobert127@gmail.com>
2021-12-18 15:12:45 -0600

    project setup

---
+++ utils/auth.js
@@ -0,0 +1,7 @@
+const withAuth = (req, res, next) => {
+  // TODO: If the user is not logged in, redirect the user to the login page
+  // TODO: If the user is logged in, allow them to view the paintings
+  if(!req.session);
+};
+
+module.exports = withAuth;
