5d72d414e203b0f937416f786c6f1d1448799c6a
Robert Warren <warrenrobert127@gmail.com>
2022-02-24 20:13:44 -0600

    auth.js

--- utils/auth.js
+++ utils/auth.js
@@ -1,7 +1,12 @@
 const withAuth = (req, res, next) => {
   // TODO: If the user is not logged in, redirect the user to the login page
   // TODO: If the user is logged in, allow them to view the paintings
-  if(!req.session);
+ //  if(!req.session);
+  if (!req.session.user_id) {
+    res.redirect("/login");
+  } else {
+    next();
+  }
 };
 
 module.exports = withAuth;
