01602769ceacd2473fbd5d577cbdfeda393866b4
Robert Warren <warrenrobert127@gmail.com>
2022-02-24 22:48:28 -0600

    comment

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,104 +1,142 @@
-const router = require('express').Router();
-const { Post, Comment, User } = require('../models');
-// TODO: Import the custom middleware
-const sequelize = require('../config/connection');
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { Post, User, Comment } = require("../models");
+const withAuth = require("../utils/auth");
 
 // get all posts for homepage
-router.get('/', (req, res) => {
-    console.log('======================');
-    Post.findAll({
+router.get("/", (req, res) => {
+  console.log("======================");
+  Post.findAll({
+    attributes: ["id", "post_content", "title", "date_created"],
+    include: [
+      {
+        model: Comment,
         attributes: [
-            'id',
-            'post_content',
-            'title',
-            'date_created',
+          "id",
+          "comment_text",
+          "post_id",
+          "user_id",
+          "date_created",
         ],
-        include: [
-            {
-                model: Comment,
-                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'date_created'],
-                include: {
-                    model: User,
-                    attributes: ['username']
-                }
-            },
-            {
-                model: User,
-                attributes: ['username']
-            }
-        ]
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+
+      res.render("homepage", {
+        posts,
+        loggedIn: req.session.loggedIn,
+      });
     })
-        .then(dbPostData => {
-            const posts = dbPostData.map(post => post.get({ plain: true }));
-
-            res.json(dbPostData)
-
-            // res.render('homepage', {
-            //     posts,
-            //     loggedIn: req.session.loggedIn
-            // });
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // get single post
-router.get('/post/:id', (req, res) => {
-    Post.findOne({
-        where: {
-            id: req.params.id
-        },
+router.get("/post/:id", (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "post_content", "title", "date_created"],
+    include: [
+      {
+        model: Comment,
         attributes: [
-            'id',
-            'post_content',
-            'title',
-            'date_created'
+          "id",
+          "comment_text",
+          "post_id",
+          "user_id",
+          "date_created",
         ],
-        include: [
-            {
-                model: Comment,
-                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'date_created'],
-                include: {
-                    model: User,
-                    attributes: ['username']
-                }
-            },
-            {
-                model: User,
-                attributes: ['username']
-            }
-        ]
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+
+      const post = dbPostData.get({ plain: true });
+
+      res.render("single-post", {
+        post,
+        loggedIn: req.session.loggedIn,
+      });
     })
-        .then(dbPostData => {
-            if (!dbPostData) {
-                res.status(404).json({ message: 'No post found with this id' });
-                return;
-            }
-
-            const post = dbPostData.get({ plain: true });
-
-            res.json(dbPostData)
-
-            // res.render('single-post', {
-            //     post,
-            //     loggedIn: req.session.loggedIn
-            // });
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
-router.get('/login', (req, res) => {
-    if (req.session.loggedIn) {
-        res.redirect('/');
-        return;
-    }
+// get all posts for dashboard
+router.get("/dashboard", withAuth, (req, res) => {
+  console.log(req.session);
+  console.log("======================");
+  Post.findAll({
+    where: {
+      user_id: req.session.user_id,
+    },
+    attributes: ["id", "post_content", "title", "date_created"],
+    include: [
+      {
+        model: Comment,
+        attributes: [
+          "id",
+          "comment_text",
+          "post_id",
+          "user_id",
+          "date_created",
+        ],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      res.render("dashboard", { posts, loggedIn: true });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
-    res.render('login');
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("login");
 });
 
 module.exports = router;
---
+++ public/js/comment.js
@@ -0,0 +1,36 @@
+async function commentFormHandler(event) {
+  event.preventDefault();
+  alert("working");
+  const comment_text = document
+    .querySelector('textarea[name="comment-body"]')
+    .value.trim();
+  const post_id = window.location.toString().split("/")[
+    window.location.toString().split("/").length - 1
+  ];
+
+  alert(comment_text);
+  alert(post_id);
+
+  if (comment_text) {
+    const response = await fetch("/api/comments", {
+      method: "POST",
+      body: JSON.stringify({
+        post_id,
+        comment_text,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.ok) {
+      document.location.reload();
+    } else {
+      alert(response.statusText);
+    }
+  }
+}
+
+document
+  .querySelector(".comment-form")
+  .addEventListener("submit", commentFormHandler);
